e87b00deaba6f6342970e104128c40b2412e08ba
tayfnet <mhmadsayfzaim@gmail.com>
2026-05-08 05:22:06 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.TAIF_PUBLIC_PRICE_CONFIG = {
+  SUPABASE_URL: "https://moafcqstydkyritatguh.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vYWZjcXN0eWRreXJpdGF0Z3VoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTEyODYsImV4cCI6MjA5MDQyNzI4Nn0.XokZCzXNeKFOwiZoWYNTuZ89QNGv-bAuOxCo9aP4Yfw",
+  RPC_NAME: "taif_public_price_display_state",
+  FALLBACK_RPC_NAME: "taif_get_currency_management_state",
+  ORG_ID: "",
+  POLL_INTERVAL_MS: 3000,
+  SHOW_STATUS: false
+};
