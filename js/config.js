082f549ec0fcfba89adbed645a4fcdf0228243b1
tayfnet <mhmadsayfzaim@gmail.com>
2026-06-04 15:53:06 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,14 @@
+window.TAIF_PUBLIC_PRICE_CONFIG = Object.freeze({
+  PROJECT_REF: "edyhwhcybhymxoadgzqb",
+  SUPABASE_URL: "https://edyhwhcybhymxoadgzqb.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_ZiJyZHW6cFlRiSUe9psBJg_W3289msN",
+  RPC_NAME: "taif_public_price_display_state",
+  WORKSPACE_ID: "default",
+  STATE_KEY: "taif-currency-management-module-v1",
+  REALTIME_ENABLED: false,
+  POLL_INTERVAL_MS: 1200,
+  REQUEST_TIMEOUT_MS: 7000,
+  SHOW_STATUS: false,
+  DEBUG: false,
+  BUILD_VERSION: "20260601-ui-layout-stable-v1"
+});
